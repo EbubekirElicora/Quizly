3aeb849f51de64fa6a66241b319d6dead5ab5305
EbubekirElicora <ebubekir.elicora@gmx.de>
2026-07-10 23:57:17 +0200

    new

--- config/settings.py
+++ config/settings.py
@@ -152,8 +152,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://127.0.0.1:5501",
-    "http://localhost:5501",
-    "https://ebubekir-elicora.developerakademie.net"
+    "http://localhost:5501"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
